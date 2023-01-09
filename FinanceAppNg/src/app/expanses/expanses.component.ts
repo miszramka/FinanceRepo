2e9b2841722b31cf828c9da9facbf2a81e1264b3
miszramka <michal.szramka90@gmail.com>
2023-01-09 12:03:23 +0100

    added material

--- FinanceAppNg/src/app/expanses/expanses.component.ts
+++ FinanceAppNg/src/app/expanses/expanses.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-expanses',
   templateUrl: './expanses.component.html',
-  styleUrls: ['./expanses.component.css']
+  styleUrls: ['./expanses.component.css'],
 })
 export class ExpansesComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
